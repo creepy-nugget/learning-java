504fc8b86642d715cbacef9afd5d18c2921e0692
creepy-nugget <Sankalpkumar9473@gmail.com>
2026-03-10 15:25:19 +0530

    Made a working class program

---
+++ classes.java
@@ -0,0 +1,9 @@
+class classes { // A class is a blueprint for an object.
+    int size;                // instance var
+    String names;         // instance var
+    int classroom_no;        // instance var
+
+    void attendence(){ // method defined under a class
+        System.out.println("Present sir!"+" "+size);
+    }
+}
---
+++ testclasses.java
@@ -0,0 +1,8 @@
+class testclasses {
+    public static void main(String[] args){
+        //Class test code goes here
+        classes c = new classes(); // making a "classes" object
+        c.size = 40;    // dot operator (.) to set size of classes
+        c.attendence(); // and to call its attendence() method or function
+    }
+}
