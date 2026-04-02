b9c096b823fd4650db14f3d0a4d30d5142c79cbd
creepy-nugget <Sankalpkumar9473@gmail.com>
2026-04-03 03:30:28 +0530

    problem on pg 88

---
+++ HFJ/chp4/btcpg88.java
@@ -0,0 +1,36 @@
+// BE THE COMPILER PG-88
+
+// PART A
+class XCopy {
+    public static void main(String[] args) {
+        int orig = 42;
+        XCopy x = new XCopy();
+        int y = x.go(orig);
+        System.out.println(orig + " " + y);
+    }
+    int go(int arg) {
+        arg = arg * 2;
+        return arg;
+    }
+}
+// Will compile , the code is correct
+
+// PART B
+
+class Clock {
+    String time;
+    void setTime(String t) {
+        time = t;
+    }
+    void getTime() { // void functions have no return values , set it to String type
+        return time;
+    }
+}
+class ClockTestDrive {
+    public static void main(String[] args) {
+        Clock c = new Clock();
+        c.setTime("1245");
+        String tod = c.getTime();
+        System.out.println("time: "+tod);
+    }
+}
