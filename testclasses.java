df828642a5b102f416fd7d0a75ced7bcea136eda
creepy-nugget <Sankalpkumar9473@gmail.com>
2026-04-03 03:28:44 +0530

    fixed some typos

--- testclasses.java
+++ testclasses.java
@@ -1,7 +1,7 @@
 class testclasses {
     public static void main(String[] args){
         //Class test code goes here
-        classes c = new classes(); // making a "classes" object
+        classes c = new classes(); // making a "classes" object with "c" as remote or reference variable
         c.size = 40;    // dot operator (.) to set size of classes
         c.name = "Sankalp";
         c.attendence(); // and to call its attendence() method or function
