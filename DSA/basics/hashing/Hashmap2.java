efbc348638c54d03e9ef6b40be8588acd1579ae3
creepy-nugget <Sankalpkumar9473@gmail.com>
2026-06-08 18:01:25 +0530

    hashing part2

---
+++ DSA/basics/hashing/Hashmap2.java
@@ -0,0 +1,28 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
+class Hashmap2{
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the string:");
+        String s =sc.next();
+
+        HashMap< Character , Integer> mp = new HashMap<>();
+
+        System.out.print("Enter the elements of arr:");
+        for(int i=0;i<s.length();i++){
+            char key = s.charAt(i);
+            mp.put(key,mp.getOrDefault(key,0)+1);
+        }
+
+        System.out.print("Enter the no. of queries q:");
+        int q = sc.nextInt();
+
+        System.out.print("Enter the queries:");
+        while(q-- >0){
+            char c = sc.next().charAt(0);
+            System.out.println(mp.getOrDefault(c, 0));
+        }
+    }
+}
