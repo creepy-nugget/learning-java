8f2424dc1b5f90ec36ea48e25e208024fd2b0e95
creepy-nugget <Sankalpkumar9473@gmail.com>
2026-06-08 17:45:23 +0530

    hashing part2

--- DSA/basics/hashing/Hashmap1.java
+++ DSA/basics/hashing/Hashmap1.java
@@ -3,19 +3,18 @@ import java.util.Scanner;
 
 class Hashmap1{
     public static void main(String[] args){
+
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the number of elements in array n:");
         int n =sc.nextInt();
+
+        HashMap< Integer, Integer> mp = new HashMap<>();
+
 
         int[] arr = new int[n];
         System.out.print("Enter the elements of arr:");
         for(int i=0;i<n;i++){
             arr[i] =sc.nextInt();
-        }
-
-        HashMap< Integer, Integer> mp = new HashMap<>();
-
-        for(int i=0;i<n;i++) {
             int key = arr[i];
             mp.put(key,mp.getOrDefault(key,0)+1);
         }
